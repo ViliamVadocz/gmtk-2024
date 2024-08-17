9c23471b20dff2ebd9252725662188fc2763f323
Lucas Holten <lhc.holten@gmail.com>
2024-08-17 19:45:53 +0200

    Fix the animations

--- src/demo/animation.rs
+++ src/demo/animation.rs
@@ -4,7 +4,7 @@
 //! - [Sprite animation](https://github.com/bevyengine/bevy/blob/latest/examples/2d/sprite_animation.rs)
 //! - [Timers](https://github.com/bevyengine/bevy/blob/latest/examples/time/timers.rs)
 
-use bevy::prelude::*;
+use bevy::{prelude::*, sprite::Anchor};
 
 use super::{
     level::{GridTick, GridTransform, WorldGrid},
@@ -32,22 +32,26 @@ impl PlayerAnimationState {
         player_assets: &PlayerAssets,
         atlas: &mut TextureAtlas,
         texture: &mut Handle<Image>,
+        anchor: &mut Anchor,
     ) {
         match self {
             Self::Idle(i) => {
                 atlas.layout = player_assets.idle_atlas.clone();
                 atlas.index = i;
                 *texture = player_assets.idle_texture.clone();
+                *anchor = Anchor::Center;
             }
             Self::Walk(i) => {
                 atlas.layout = player_assets.walk_atlas.clone();
                 atlas.index = i;
                 *texture = player_assets.walk_texture.clone();
+                *anchor = Anchor::Custom(Vec2::new(-0.25, 0.));
             }
             Self::Climb(i) => {
                 atlas.layout = player_assets.climb_atlas.clone();
                 atlas.index = i;
                 *texture = player_assets.climb_texture.clone();
+                *anchor = Anchor::Custom(Vec2::new(-0.25, -0.25));
             }
         };
     }
@@ -67,22 +71,27 @@ fn propagate_grid_transform(
     player_assets: Option<Res<PlayerAssets>>,
 ) {
     for (mut transform, pos, state, mut atlas, mut sprite, mut texture) in &mut q {
-        if let Some(anim) = &state.animation {
+        let anim_state = if let Some(anim) = &state.animation {
             let frame = (anim.func)(tick.0.fraction());
-            frame.state.update_animation(
-                player_assets
-                    .as_ref()
-                    .expect("Player assets should already be loaded."),
-                &mut atlas,
-                &mut texture,
-            );
+
             let new = grid.project_to_world(pos.0.as_vec2() + frame.offset(state.x_dir));
             transform.translation = new.extend(transform.translation.z);
+            frame.state
         } else {
             transform.translation = grid
                 .project_to_world(pos.0.as_vec2())
                 .extend(transform.translation.z);
-        }
+            PlayerAnimationState::Idle(0)
+        };
+        anim_state.update_animation(
+            player_assets
+                .as_ref()
+                .expect("Player assets should already be loaded."),
+            &mut atlas,
+            &mut texture,
+            &mut sprite.anchor,
+        );
         sprite.flip_x = state.x_dir == -1;
+        sprite.anchor = Anchor::Custom(sprite.anchor.as_vec() * Vec2::new(state.x_dir as f32, 1.));
     }
 }
